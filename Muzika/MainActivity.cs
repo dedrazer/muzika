2b4d4bfbf263553956247a0d08bd701a92e4c6a9
dedrazer <martinmeli96@gmail.com>
2018-10-12 08:27:47 +0200

    init

---
+++ Muzika/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using static Android.Views.View;
+using System;
+using Android.Views;
+
+namespace Muzika
+{
+    [Activity(Label = "Muzika", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private bool IsDrawAGridHidden = false;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            View v_Overlay = FindViewById<View>(Resource.Id.v_Overlay);
+
+            v_Overlay.Touch += new EventHandler<TouchEventArgs>(OverlayTouch);
+
+            v_Overlay.Drag += new EventHandler<DragEventArgs>(OverlayDrag);
+        }
+
+        /// <summary>
+        /// capture overlay Touch event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OverlayTouch(object sender, TouchEventArgs e)
+        {
+            if (!IsDrawAGridHidden)
+            {
+                TextView tv_DrawAGrid = FindViewById<TextView>(Resource.Id.tv_DrawAGrid);
+
+                ViewMethods.Display.Hide(tv_DrawAGrid);
+
+                IsDrawAGridHidden = true;
+            }
+        }
+
+        /// <summary>
+        /// capture overlay Drag event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OverlayDrag(object sender, DragEventArgs e)
+        {
+            View v_GridDrawer = FindViewById<View>(Resource.Id.v_GridDrawer);
+
+            v_GridDrawer.Visibility = ViewStates.Visible;
+        }
+    }
+}
+
---
+++ View/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViewMethods
+{
+    public static class Display
+    {
+        /// <summary>
+        /// hides given view
+        /// </summary>
+        /// <param name="view"></param>
+        public static void Hide(Android.Views.View view)
+        {
+            view.Visibility = Android.Views.ViewStates.Invisible;
+        }
+    }
+}
