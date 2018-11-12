4427e104e6718f94c60f52bddf2341819af11841
dedrazer <martinmeli96@gmail.com>
2018-11-12 21:42:25 +0100

    added new classes

---
+++ Muzika/Frequency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Muzika
+{
+    public static class Frequency
+    {
+        //number used to calculate key frequency
+        private const double twelveRootTwo = 1.0594630943592952646;
+
+        //converts the number of tones below A4 into a frequency
+        public static float CalculateFrequency(short tonesBelowA4)
+        {
+            short n = TonesToSemiTomesBelowA4(tonesBelowA4);
+
+            return 440 * (float)Math.Pow(twelveRootTwo, -n);
+        }
+
+        //converts tones into semi tones in relation to and less than A4
+        public static short TonesToSemiTomesBelowA4(short tonesFromA4)
+        {
+            short n = 0;
+            for (short i = 0; i < tonesFromA4; i++)
+            {
+                if (i % 12 == 2 || i % 12 == 5)
+                {
+                    n++;
+                }
+                else
+                {
+                    n += 2;
+                }
+            }
+
+            return n;
+        }
+    }
+}
---
+++ MuzikaClasses/Neighbourhood.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MuzikaClasses
+{
+    class Neighbourhood
+    {
+        public bool[] Neighbours { get; private set; } = new bool[9];
+
+        /// <summary>
+        /// create a Neighbourhood given the source array and the centre co-ordinates
+        /// </summary>
+        /// <param name="source">source grid</param>
+        /// <param name="x">centre x</param>
+        /// <param name="y">centre y</param>
+        public Neighbourhood(bool[,] source, short x, short y)
+        {
+            //minimum size 3x3
+            if (source.GetLength(0) > 2 && source.GetLength(1) > 2)
+            {
+                short[] xCoOrdinates = new short[3];
+                short[] yCoOrdinates = new short[3];
+                //overflow
+                if (x == 0)
+                {
+                    xCoOrdinates = new short[] {
+                        (short)(source.GetLength(0) - 1),
+                        0,
+                        1
+                    };
+                }
+                else if (x == source.GetLength(0) - 1)
+                {
+                    xCoOrdinates = new short[] {
+                        (short)(x-1),
+                        x,
+                        0,
+                    };
+                }
+                else
+                {
+                    xCoOrdinates = new short[] {
+                        (short)(x-1),
+                        x,
+                        (short)(x+1)
+                    };
+                }
+                if (y == 0)
+                {
+                    yCoOrdinates = new short[] {
+                        (short)(source.GetLength(1) - 1),
+                        0,
+                        1
+                    };
+                }
+                else if (y == source.GetLength(1) - 1)
+                {
+                    yCoOrdinates = new short[] {
+                        (short)(y-1),
+                        y,
+                        0,
+                    };
+                }
+                else
+                {
+                    yCoOrdinates = new short[] {
+                        (short)(y-1),
+                        y,
+                        (short)(y+1)
+                    };
+                }
+
+                int i = 0;
+                foreach (short yCO in yCoOrdinates)
+                {
+                    foreach (short xCO in xCoOrdinates)
+                    {
+                        Neighbours[i] = source[xCO, yCO];
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
